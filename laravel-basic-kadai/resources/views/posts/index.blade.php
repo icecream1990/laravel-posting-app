a226decf88eff83568f32a081c83cfe0bd73abda
yuma.m441@gmail.com <yuma.m441@gmail.com>
2024-10-05 21:43:50 +0900

    laravel-basic-kadaiを追加

---
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Laravel課題</title>
+ </head>
+
+ <body>
+  <header>Laravel課題</header>
+  <main>
+  <h1>投稿一覧</h1>
+  </main>
+ </body>
+</html>
