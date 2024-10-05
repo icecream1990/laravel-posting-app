b30aa76c5c71d45a5dc7ae770e9b6b87510644dc
yuma.m441@gmail.com <yuma.m441@gmail.com>
2024-10-05 23:49:03 +0900

    課題の提出

--- laravel-basic-kadai/resources/views/posts/index.blade.php
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -8,9 +8,25 @@
  </head>
 
  <body>
-  <header>Laravel課題</header>
-  <main>
-  <h1>投稿一覧</h1>
-  </main>
+    <h1>投稿一覧</h1>
+     <table>
+         <tr>
+             <th>ID</th>
+             <th>タイトル</th>
+             <th>本文</th>
+             <th>作成日時</th>
+             <th>更新日時</th>
+         </tr>
+         @foreach($posts as $post)
+             <tr>
+                 <td>{{ $post->id }}</td>
+                 <td>{{ $post->title }}</td>
+                 <td>{{ $post->content }}</td>
+                 <td>{{ $post->created_at }}</td>
+                 <td>{{ $post->updated_at }}</td>
+             </tr>            
+         @endforeach       
+     </table>
  </body>
-</html>
+ 
+ </html>
